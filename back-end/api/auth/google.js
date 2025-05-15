bdff51bd225895a6b13b5c9684409bdd69f935c9
marchel-24 <riojuntak86@gmail.com>
2025-05-15 20:17:53 +0700

    Update google.js

--- back-end/api/auth/google.js
+++ back-end/api/auth/google.js
@@ -2,7 +2,7 @@ import { googleLogin } from '../../controllers/authController';
 const applyCors = require("../_utils/cors");
 
 export default async function handler(req, res) {
-    if (applyCors(res,req)) return;
+    if (applyCors(req,res)) return;
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method Not Allowed' });
   }
